aa1a407722ee5f3f2a3fb7d432cb81c5fdd20ae9
Sailee Shingare <56690599+saileeshingare@users.noreply.github.com>
2025-09-29 15:07:16 -0500

    Create SearchResult.java

---
+++ Assignment1/Source/SearchResult.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchResult {
+    int cellsSearched = 0;
+    List<Cell> carrierCoords = new ArrayList<>();
+    List<Cell> submarineCoords = new ArrayList<>();
+}
